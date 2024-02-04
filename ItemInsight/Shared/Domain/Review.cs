c3280f7d9a589f5dce6d09ce58994fcc135254a3
JEREMY NG WEI CHENG 2202339D <2202339D@student.tp.edu.sg>
2024-02-04 16:16:18 +0800

    Finished Review CRUD entirely fixed

--- ItemInsight/Shared/Domain/Review.cs
+++ ItemInsight/Shared/Domain/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ItemInsight.Shared.Domain
     public class Review : BaseDomainModel
     {
         public string? ConsumerReview { get; set; }
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public double? Rating { get; set; }
         public int ProductId { get; set; }
         public virtual Product? Product { get; set; }
